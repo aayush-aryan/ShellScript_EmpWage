bb8f3da86b0d09c243fd37c970929d03a7a6e780
aayush-aryan <log2aayusharyan@gmail.com>
2020-04-25 18:38:41 +0530

    [aayush]added. using Random function to check employee is present or not

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,2 +1,9 @@
 #!/bin/bash -x
-echo "****** WELCOME TO EMPLOYEE WAGE COMPUTATION ******"
+IS_PRESENT=1
+empCheck=$((RANDOM%2))
+if [ $IS_PRESENT -eq $empCheck ]
+then
+       echo Employee is present
+else
+       echo Employee is absent
+fi
