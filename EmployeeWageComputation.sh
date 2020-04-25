df183b322da0948107b1bafc8ec305849f2ad31e
aayush-aryan <log2aayusharyan@gmail.com>
2020-04-25 19:11:43 +0530

    [aayush]added. adding feature for part time employee

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,12 +1,16 @@
 #!/bin/bash -x
 WAGE_PER_HRS=20
 IS_FULL_TIME=1
-empCheck=$((RANDOM%2))
+IS_PART_TIME=2
+empCheck=$((RANDOM%3))
 if [ $IS_FULL_TIME -eq $empCheck ]
 then
        empHrs=8
+elif [ $IS_PART_TIME -eq $empCheck ]
+then
+       empHrs=4
 else
-       empHrs=0
+		 empHrs=0
 fi
 dailyWage=$(( $empHrs * $WAGE_PER_HRS ))
 echo $dailyWage
